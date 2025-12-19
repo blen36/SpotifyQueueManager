ba313e76982ea64b92cdaa9471eec746a99268d6
Oleg Roslik <lolkeker.207@gmail.com>
2025-12-19 20:00:09 +0300

    added style to room, added checking email replay

--- spotify_jukebox/jukebox/forms.py
+++ spotify_jukebox/jukebox/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Room
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError # Обязательно для проверки почты
 
 class CreateRoomForm(forms.ModelForm):
     class Meta:
@@ -12,8 +13,15 @@ class JoinRoomForm(forms.Form):
     code = forms.CharField(label='Code', max_length=8)
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField() # Добавляем поле email, чтобы знать почту друзей
+    email = forms.EmailField(required=True) # Делаем поле обязательным
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email'] # Здесь НЕ должно быть слова return
+
+    # Метод проверки уникальности почты
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Пользователь с такой почтой уже зарегистрирован.")
+        return email
